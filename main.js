6d6003760ccd99490b9ae7e2603b33e6c4d1a0af
GUILHERME SERAFIM DE LIMA <guilherme.serafim@alunoescolassociais.org.br>
2024-11-01 08:39:35 -0300

    começei o README

---
+++ main.js
@@ -0,0 +1,22 @@
+const apiKey = '356a1cbfe6e284549ede9c05';
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+
+// função para consulta a taxa de cambio via API 
+async function getExchangeRete(deMoeda, paraMoeda){
+    // trata erro try 
+    try{
+        const response = await fetch(`${apiURL}${deMoeda}`);
+        const data = response.json();
+
+        if(data.result ==="success"){
+            return data.conversion_rates[paraMoeda];
+        }else{
+            throw new Error("erro ao buscar tava de cambio")
+        }
+    }catch (error){
+        console.error("Erro:", error);
+        return null;
+
+    }
+}
